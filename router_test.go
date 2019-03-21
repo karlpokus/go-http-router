8491c4ddeced0d28d2fcabb868dc4d9aa7785c55
Karl Pokus <karl.pokus@gmail.com>
2019-03-21 22:22:20 +0100

    add testtable

--- router_test.go
+++ router_test.go
@@ -8,23 +8,34 @@ import (
 )
 
 func foo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
+	w.Write([]byte("foo"))
+}
+
+var testTable = []struct {
+	path   string
+	status int
+	body   string
+}{
+	{"/", 200, "foo"},
+	{"/missing", 404, "Not Found\n"},
 }
 
 func TestRouter(t *testing.T) {
-	r := New()
+	var r = New()
 	r.Handler("/", http.HandlerFunc(foo))
 
-	req := httptest.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-	res := w.Result()
-	body, _ := ioutil.ReadAll(res.Body)
+	for _, tt := range testTable {
+		req := httptest.NewRequest("GET", tt.path, nil)
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		res := w.Result()
+		body, _ := ioutil.ReadAll(res.Body)
 
-	if res.StatusCode != 200 {
-		t.Errorf("statuscode want %d, got %d", 200, res.StatusCode)
-	}
-	if string(body) != "hello" {
-		t.Errorf("body want %s, got %s", "hello", string(body))
+		if res.StatusCode != tt.status {
+			t.Errorf("statuscode err: want %d, got %d", tt.status, res.StatusCode)
+		}
+		if string(body) != tt.body {
+			t.Errorf("body err: want %s, got %s", tt.body, string(body))
+		}
 	}
 }
