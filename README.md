f4041e3a733320bcc6ed148720f4413ba821cf04
Karl Pokus <karl.pokus@gmail.com>
2019-03-25 16:14:55 +0100

    add static routes benchmark

--- README.md
+++ README.md
@@ -4,10 +4,18 @@ Benchmarking a custom http router in go. Let's see how fast I can make it compar
 # usage
 ```bash
 $ go test -bench=.
+
+goos: darwin
+goarch: amd64
+pkg: github.com/karlpokus/go-http-router
+BenchmarkRouter-4   	  100000	     15837 ns/op	       0 B/op	       0 allocs/op
 ```
+
+I tried to reproduce the testing conditions in [the router benchmark repo](https://github.com/julienschmidt/go-http-routing-benchmark) but seeing as this beats the http.DefaultServeMux - on both speed and allocations - I think it's probably too good to be true. I'll investigate further later.
 
 # todos
 - [x] add method to route struct
+- [x] benchmark static routes
 - [ ] add url parameters
 
 # license
--- router_test.go
+++ router_test.go
@@ -5,11 +5,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-)
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("foo"))
-}
+	"github.com/karlpokus/go-http-router/testdata/static"
+	"github.com/karlpokus/go-http-router/testdata/mock"
+)
 
 var testTable = []struct {
 	method string
@@ -23,13 +22,13 @@ var testTable = []struct {
 }
 
 func TestRouter(t *testing.T) {
-	r := New()
-	r.Handler("GET", "/", http.HandlerFunc(foo))
+	rtr := New()
+	rtr.Handler("GET", "/", http.HandlerFunc(mock.Foo))
 
 	for _, tt := range testTable {
-		req := httptest.NewRequest(tt.method, tt.path, nil)
+		r := httptest.NewRequest(tt.method, tt.path, nil)
 		w := httptest.NewRecorder()
-		r.ServeHTTP(w, req)
+		rtr.ServeHTTP(w, r)
 		res := w.Result()
 		body, _ := ioutil.ReadAll(res.Body)
 
@@ -42,16 +41,26 @@ func TestRouter(t *testing.T) {
 	}
 }
 
-var benchRes http.Handler // avoiding compiler optimisations
-
 func BenchmarkRouter(b *testing.B) {
-	var res http.Handler
-	r := New()
-	foo := http.HandlerFunc(foo)
-	r.Handler("GET", "/", foo)
-	for n := 0; n < b.N; n++ {
-		//r.ServeHTTP(w, req)
-		res = r.find("GET", "/missing")
+	rtr := New()
+	for _, r := range static.Routes {
+		rtr.Handler(r.Method, r.Path, http.HandlerFunc(mock.Noop))
 	}
-	benchRes = res
+
+	w := new(mock.ResponseWriter)
+	r := httptest.NewRequest("GET", "/", nil)
+	u := r.URL
+	rq := u.RawQuery
+
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		for _, staticroute := range static.Routes {
+			r.Method = staticroute.Method
+			r.RequestURI = staticroute.Path
+			u.Path = staticroute.Path
+			u.RawQuery = rq
+
+			rtr.ServeHTTP(w, r)
+		}
+	}
 }
